b449b1b2457e53d3b42d72d2854bb3805d1fce2b
Yaradla <yrdls@7831c1c4979a.ant.amazon.com>
2016-01-12 21:52:33 +0530

    test service3 commit

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('fetch', function(event) {
  
     var fetchRequest = event.request.clone();
 
-        return event.respondWith( return fetch(fetchRequest).then(
+         event.respondWith(fetch(fetchRequest).then(
           function(response) {
             // Check if we received a valid response
             if(!response || response.status !== 200 || response.type !== 'basic') {
